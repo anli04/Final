54a6ebc44001372ecbf4613caccc3b035eab3397
Anthony Li <ali04@stuy.edu>
2020-01-04 01:29:29 -0500

    basics

--- funcs.c
+++ funcs.c
@@ -24,8 +24,6 @@ int choose(char * choices){
     i = 0;
     fgets(input, 5, stdin);
     errcheck("with input");
-    char c;
-    while ((c = getc(stdin)) != '\n' && c != EOF) // clear stdin in case of excess chars
     input[4] = 0;
     if (strchr(input, '\n')) *strchr(input, '\n') = 0;
     while(args[i]){
