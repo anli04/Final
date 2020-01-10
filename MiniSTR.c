bb54f8710beae4cde2699965cb00b556803c66c1
jlin03 <jlin03@stuy.edu>
2020-01-10 10:21:58 -0500

    framework for strength game coded

--- MiniSTR.c
+++ MiniSTR.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 int game() {
-  char * f1 = "";
+  char * f1 = "       ___\n      (   )\n       ‾|‾\n     /‾‾|‾‾\\\n❚█═════════════█❚\n        |\n       / \\\n      /   \\";
+  char * f2 = "       ___\n      (   )\n❚█═════════════█❚\n     \\  |  /\n      ‾‾|‾‾\n        |\n       / \\\n      /   \\";
+  char* clear[1];
+  clear[0] = malloc(sizeof(char)*16);
+  strcpy(clear[0],"clear");
+  char temp[32];
+  char t;
+  int reps = 0;
 
 
-  while(fgets)
+
+  while(1) {
+    system("clear");
+    printf("\n%s",f1);
+
+    fgets(temp,32,stdin);
+    if(strcmp(temp,"]\n") == 0) {
+      reps++;
+    }
+    else {
+      break;
+    }
+    system("clear");
+    printf("\n%s",f2);
+    fgets(temp,32,stdin);
+    if(strcmp(temp,"]\n") == 0) {
+      reps++;
+    }
+    else {
+      break;
+    }
 
 
+  }
 
 
+}
 
-
-
+int main(){
+  game();
 }
